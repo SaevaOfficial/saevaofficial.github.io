72e3628c29b7f37ac47a0e99b369d2177bbca568
Eienwq <56482606+Eienwq@users.noreply.github.com>
2021-10-27 22:38:06 +0300

    🌙
    🌙

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 // Bu Proje Eienwq Tarafından Yapılmıştır.
 
-var btnSelam=document.getElementById("button-link");
+var btnSelam=document.getElementById("eienwq-link");
 	btnSelam.onclick=function(){
 		window.alert("Discord Profiline Aktarılıyorsunuz...");
         window.location.href = 'https://discord.com/users/323095557470158855';
