9609f918c3919f5d990dcec4ea36a8fabb000ea2
Eienwq <56482606+Eienwq@users.noreply.github.com>
2021-10-27 22:32:35 +0300

    Create script.js

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+// Bu Proje Eienwq Tarafından Yapılmıştır.
+
+var btnSelam=document.getElementById("button-link");
+	btnSelam.onclick=function(){
+		window.alert("Discord Profiline Aktarılıyorsunuz...");
+        window.location.href = 'https://discord.com/users/323095557470158855';
+	}
